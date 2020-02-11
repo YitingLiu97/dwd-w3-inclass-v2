7bda120735dc7aff143f882d2ad8ea73ae35c896
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2020-02-10 20:00:15 -0500

    buttonhandler, randomize background color with alpha in main.js

--- main.js
+++ main.js
@@ -3,13 +3,57 @@
 //to preven javascript from loading before the index.html
 // should use event handler
 
-
-
-window.addEventlistener('DOMContentLoaded',()=>{ 
 //DOM API 
-const paragraph = document.createElement("p");
-paragraph.textContent="Hello from Javascript";
-});
+//click the button and add an element on the page 
+//bind the onlcick event handler
+
+/***click the button and ADD an element on the page **/
+// window.addEventListener('DOMContentLoaded',()=>{ 
+
+// const button = document.getElementById("add-button");
+// button.onclick = function (){
+// const paragraph = document.createElement("p");
+// paragraph.textContent="Hello from Javascript";
+// document.body.appendChild(paragraph);
+// };
+// });
+
+
+/***click the button and REMOVE an element on the page **/
+window.addEventListener('DOMContentLoaded',()=>{ 
+const button = document.getElementById("fun-button");
+// button.onclick = function (){
+    /**remove div - method 1 **/
+
+//     button.parentElement.remove();
+
+// };
+
+/***remove div - method 2 ***/
+button.onclick =buttonHandler;
+});    
+
+//nice way of reusing functions 
+function buttonHandler(event){
+
+    const r= Math.random()*255;
+    const g= Math.random()*255;
+    const b= Math.random()*255;
+    const a= Math.random();
+
+    //the element this event is called on 
+    // event.target.parentElement.remove();
+
+    //template strings 
+    event.target.parentElement.style.backgroundColor= `rgba(${r},${g},${b},${a})`;
+
+}
+
+function randomColor(){
+
+
+
+}
 
 
 
